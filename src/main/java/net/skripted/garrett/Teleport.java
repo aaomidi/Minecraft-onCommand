4784affb08f825a3ba5236fe445e41162c0f3c3f
Amir Omidi <aa_omidi@yahoo.com>
2014-04-30 10:32:57 +0430

    Update Teleport.java
    Changed it a bit :)

--- src/main/java/net/skripted/garrett/Teleport.java
+++ src/main/java/net/skripted/garrett/Teleport.java
@@ -21,6 +21,7 @@ import java.util.ArrayList;
  */
 public final class Teleport extends JavaPlugin
 {
+	PluginDescriptionFile pdfFile;
 	@Override
 	// On server startup
 	public void onEnable()
@@ -28,28 +29,36 @@ public final class Teleport extends JavaPlugin
 		// this refers to this class
 		// pdfFile.getName() is getting the defined name from plugin.yml
 		// in this case it's Practice
-		PluginDescriptionFile pdfFile = this.getDescription();
-		getLogger().info(pdfFile.getName() + " has been enabled!");
+		pdfFile=this.getDescription();
+		//Instead of initiating pdfFile twice. Do what i did at line 24 and 32
+		//PluginDescriptionFile pdfFile = this.getDescription();
+		//This isn't required, Bukkit handles messages.
+		//getLogger().info(pdfFile.getName() + " has been enabled!");
 	}
 
 	// On server stop/reload
 	@Override
 	public void onDisable()
 	{
+		
 		PluginDescriptionFile pdfFile = this.getDescription();
-		getLogger().info(pdfFile.getName() + " has been disabled!");
+		//Nor is this.
+		//getLogger().info(pdfFile.getName() + " has been disabled!");
 	}
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String args[])
 	{
 		//Command, doesn't matter if random, RANDOM, RaNdOm, etc
-		if(commandLabel.equalsIgnoreCase("random"))
+		//use cmd.getName() so
+		//if(commandLabel.equalsIgnoreCase("random"))
+		if(cmd.getName.equalsIgnoreCase("random")
 		{
 			//Casting player to sender, final because Zenexer taught me the ways :p
 			final Player player = (Player) sender;
 
 			//Math.random() is getting a random number between 0 and 1000
 			//casting (Math.random() * 1000) to an integer instead of a float
+			//You can make a Random object Random rand=new Random(); (kinda useful when defning boundaries)
 			int X = (int) (Math.random() * 1000);
 			int Z = (int) (Math.random() * 1000);
 
@@ -64,8 +73,14 @@ public final class Teleport extends JavaPlugin
 
 			//Player message on the final random teleport location
 			player.sendMessage(ChatColor.GREEN + "You were teleported to X: " + X + "Y: " + Y + "Z: " + Z);
+			//Another way to do colors and messages:
+			player.sendMessage(ChatColor.translateAlternateColorCodes('&', String.format("&aYou were teleported to X:%s Y:%s Z%s", X,Y,Z )
+			//Return true when player does the stuff right.
+			return true;
 		}
 		//Boolean returned true onCommand
-		return true;
+		//Don't return true by default.
+		//return true;
+		return false;
 	}
 }
